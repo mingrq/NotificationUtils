e7fcd31c2bba80185b10ec69128ca8fbb0cefb03
002 <mingruqi@sina.cn>
2019-11-28 10:39:40 +0800

    Initial commit

---
+++ notificationutillib/src/main/java/com/ming/notificationutillib/NotificationUtil.java
@@ -0,0 +1,14 @@
+package com.ming.notificationutillib;
+
+import android.content.Context;
+import android.content.ContextWrapper;
+
+/**
+ * 通知工具
+ */
+public class NotificationUtil extends ContextWrapper {
+
+    public NotificationUtil(Context base) {
+        super(base);
+    }
+}
