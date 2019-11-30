aedb838f0eef34105448c593acbb86581b57f1de
002 <mingruqi@sina.cn>
2019-11-30 18:10:41 +0800

    tb

--- app/src/main/java/com/ming/notificationutil/MainActivity.java
+++ app/src/main/java/com/ming/notificationutil/MainActivity.java
@@ -3,6 +3,10 @@ package com.ming.notificationutil;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.ming.notificationutillib.StatusBarNotificationUtil;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final StatusBarNotificationUtil statusBarNotificationUtil = new StatusBarNotificationUtil(this);
+        /**
+         * 普通通知
+         */
+        final int[] i = {1};
+        Button simpleNotification = findViewById(R.id.id_simpleNotification);
+        simpleNotification.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                statusBarNotificationUtil.sendSimpleNotification(i[0],"asdfsda","sdfas",R.mipmap.ic_launcher);
+                i[0]++;
+            }
+        });
     }
 }
--- notificationutillib/src/main/java/com/ming/notificationutillib/StatusBarNotificationUtil.java
+++ notificationutillib/src/main/java/com/ming/notificationutillib/StatusBarNotificationUtil.java
@@ -4,8 +4,13 @@ import android.app.Notification;
 import android.app.NotificationManager;
 import android.content.Context;
 import android.content.ContextWrapper;
+import android.content.pm.VersionedPackage;
+import android.os.Build;
 
 import androidx.core.app.NotificationCompat;
+
+import static androidx.core.app.NotificationCompat.DEFAULT_VIBRATE;
+import static androidx.core.app.NotificationCompat.PRIORITY_DEFAULT;
 
 /**
  * 状态栏通知工具
@@ -18,11 +23,40 @@ public class StatusBarNotificationUtil extends ContextWrapper {
 
     /**
      * 发送简单通知
+     * <p>
+     * 最基本、精简形式（也称为折叠形式）的通知会显示一个图标、一个标题和少量内容文本。在本节中，您将学习如何创建用户单击后可在应用中启动 Activity 的通知。
      *
-     *最基本、精简形式（也称为折叠形式）的通知会显示一个图标、一个标题和少量内容文本。在本节中，您将学习如何创建用户单击后可在应用中启动 Activity 的通知。
+     * @param notifyId 通知Id
+     * @param title 通知标题
+     * @param content 通知内容
+     * @param icon 通知图标
+     *
      */
-    public void sendSimpleNotification(){
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
-
+    public void sendSimpleNotification(int notifyId, String title, String content , int icon ) {
+       sendSimpleNotification(notifyId, title, content, icon,PRIORITY_DEFAULT);
     }
+
+    /**
+     * 发送简单通知
+     * <p>
+     * 最基本、精简形式（也称为折叠形式）的通知会显示一个图标、一个标题和少量内容文本。在本节中，您将学习如何创建用户单击后可在应用中启动 Activity 的通知。
+     *
+     * @param notifyId 通知Id
+     * @param title 通知标题
+     * @param content 通知内容
+     * @param icon 通知图标
+     *@param priority 通知优先级
+     */
+    public void sendSimpleNotification(int notifyId, String title, String content , int icon ,int priority) {
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(getApplicationContext());
+        builder.setPriority(priority);
+        builder.setContentTitle(title);
+        builder.setContentText(content);
+        builder.setSmallIcon(icon);
+        builder.setOnlyAlertOnce(false);
+        builder.setOngoing(false);
+        Notification build = builder.build();
+        ((NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE)).notify(notifyId, build);
+    }
+
 }
